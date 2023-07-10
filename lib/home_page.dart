40f432884f9caca07e6c8786c0205277c2bde9de
Pawan Dev <pdev4276@gmail.com>
2023-07-10 21:00:53 +0530

    Day2 Completed Buddy

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,17 +2,18 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-  final int days = 20;
-  final String name = "Codepur";
+  final int days = 50;
+  final String name = "CodePur";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Catalog App')),
-      body: Center(
-          child: Container(
-        child: Text("Welcome to ${days} days of flutter by ${name}"),
-      )),
-      drawer:Drawer(),
-    );
+        appBar: AppBar(
+          title: Text("Catalog App"),
+        ),
+        body: Center(
+            child: Container(
+            child: Text("Welcome to $days of flutter by $name"),
+        )));
   }
 }
